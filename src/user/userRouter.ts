d904761787aa5ef118ff1439995341241a6478c7
José Carlos <jose.carlos@fdez.se>
2025-01-15 13:50:03 +0100

    Agregar controlador para crear usuarios en userRouter y validar el cuerpo de la solicitud

--- src/user/userRouter.ts
+++ src/user/userRouter.ts
@@ -1,5 +1,11 @@
-import { Elysia } from "elysia";
+import { Elysia, t } from "elysia";
+import { createUserController } from "@/server/dependecies";
 
 export const userRouter = new Elysia({prefix: "/user"})
-  .post('/', () => "Create a user")
+  .post('/', createUserController.run.bind(createUserController), {
+    body: t.Object({
+      email: t.String(),
+      password: t.String()
+    })
+  })
   .get('/', () => "Get all users")
