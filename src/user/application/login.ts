7c74ccc038bd5be16d480ac93e1f278fd9802212
José Carlos <jose.carlos@fdez.se>
2025-01-15 16:25:10 +0100

    Agregar clase LoginUser para gestionar el inicio de sesión de usuarios

---
+++ src/user/application/login.ts
@@ -0,0 +1,21 @@
+import type { IUser } from "../domain/IUser";
+import type { IHash } from "@/services/interfaces/IHash";
+
+export class LoginUser {
+  constructor(
+    private userRepository: IUser,
+    private hash: IHash
+  ) {}
+
+  async run(email: string, password: string) {
+    const user = await this.userRepository.find(email);
+    if (!user) {
+      return new Error("User not found");
+    }
+    const isValid = await this.hash.compare(password, user.password);
+    if (!isValid) {
+      return new Error("Invalid password");
+    }
+    return user;
+  }
+}
