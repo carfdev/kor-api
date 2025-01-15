1c9203c38f99e3f2c8ab0426da7e030850c27255
José Carlos <jose.carlos@fdez.se>
2025-01-15 19:11:05 +0100

    Mejorar el controlador de inicio de sesión para manejar respuestas con set y agregar generación de tokens de acceso y actualización

--- src/user/infraestructure/controllers/loginController.ts
+++ src/user/infraestructure/controllers/loginController.ts
@@ -2,26 +2,45 @@ import { LoginUser } from "@/user/application/login";
 
 export class LoginUserController {
   constructor(private loginUser: LoginUser) {}
-  async run({ body, error }: { body: {email: string, password: string}, error: any}) {
+  async run({ body, access, refresh, set, cookie: { auth } }: { body: {email: string, password: string}, access: any, refresh: any, set: any, cookie: any }) {
     try {
       const user = await this.loginUser.run(body.email, body.password);
       if (user instanceof Error) {
         if (user.message === "User not found") {
-          return error(404, {
+          set.status = 404;
+          return {
             message: "User not found"
-          })
+          }
         }
         if (user.message === "Invalid password") {
-          return error(401, {
+          set.status = 401;
+          return {
             message: "Invalid password"
-          })
+          }
         }
       } else{
 
       const { password, ...userWithoutPassword } = user;
 
+        const access_token = await access.sign({
+          id: user.id
+        });
+
+        const refresh_token = await refresh.sign({
+          id: user.id
+        });
+
+        auth.set({
+          value: refresh_token,
+          httpOnly: true,
+          maxAge: 30 * 86400,
+          path: '/',
+      })
+
+
         return {
           data: {
+            token: access_token,
             user: userWithoutPassword,
             message: "Login successful"
           }
@@ -29,11 +48,11 @@ export class LoginUserController {
       }
       
     } catch (e) {
-      const err = e as Error;
-
-      return error(500, {
-        message: err.message
-      })
+      const error = e as Error;
+      set.status = 500;
+      return {
+        message: error.message
+      }
     }
   }
 }
