048a858afffd25a1907a616ddb77be7076a9b806
José Carlos <jose.carlos@fdez.se>
2025-01-15 21:00:34 +0100

    Agregar inyección de dependencia para RefreshToken en RefreshTokenController

--- src/user/infraestructure/controllers/refreshTokenControler.ts
+++ src/user/infraestructure/controllers/refreshTokenControler.ts
@@ -1,5 +1,7 @@
+import { RefreshToken } from "@/user/application/refreshToken";
+
 export class RefreshTokenController {
-  constructor() {}
+  constructor(private refreshToken: RefreshToken) {}
 
   async run({ access, refresh, set, cookie: { auth }}: { access: any, refresh: any, set: any, cookie: any }) {
     try {
@@ -10,6 +12,17 @@ export class RefreshTokenController {
           message: "Invalid refresh token"
         }
       }
+
+
+      const dbRefreshToken = await this.refreshToken.run(id, auth.value);
+
+      if (dbRefreshToken instanceof Error) {
+        set.status = 401;
+        return {
+          message: dbRefreshToken.message
+        }
+      }
+      
       const access_token = await access.sign({ id });
       set.status = 200;
       return {
