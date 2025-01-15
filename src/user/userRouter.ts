6b360b32de6227313013ea883bf47e6cd6e09c51
José Carlos <jose.carlos@fdez.se>
2025-01-15 19:10:53 +0100

    Agregar soporte para JWT en userRouter y controlador de actualización de token

--- src/user/userRouter.ts
+++ src/user/userRouter.ts
@@ -1,9 +1,23 @@
 import { Elysia } from "elysia";
 import { createUserController } from "@/server/dependecies";
 import { loginUserController } from "@/server/dependecies";
+import { refreshTokenController } from "@/server/dependecies";
 import { createUserDTO } from "./domain/userDTO";
-
-export const userRouter = new Elysia({prefix: "/user"})
-  .post('/register', createUserController.run.bind(createUserController), createUserDTO)
-  .post('/login', loginUserController.run.bind(loginUserController), createUserDTO)
+import { jwt } from '@elysiajs/jwt'
+export const userRouter = new Elysia({
+  prefix: "/user"
+})
+  .use(jwt({
+    name: "access",
+    secret: process.env.JWT_SECRET_ACCESS as string,
+    exp: '1h'
+  }))
+  .use(jwt({
+    name: "refresh",
+    secret: process.env.JWT_SECRET_REFRESH as string,
+    exp: '30d'
+  }))
+  .post('/register', (ctx) => createUserController.run(ctx), createUserDTO)
+  .post('/login', (ctx) => loginUserController.run(ctx), createUserDTO)
+  .get('/refresh', (ctx) => refreshTokenController.run(ctx))
   .get('/', () => "Get all users")
