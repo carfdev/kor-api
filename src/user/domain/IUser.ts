a5431b3934525f82986d0d5dc239ed578d130ee9
José Carlos <jose.carlos@fdez.se>
2025-01-15 21:00:16 +0100

    Agregar métodos para manejar tokens de actualización en la interfaz IUser

--- src/user/domain/IUser.ts
+++ src/user/domain/IUser.ts
@@ -1,6 +1,8 @@
 import { User } from "./User";
 
 export interface IUser {
-  create(email: string, password: string): Promise<User>;
+  create(email: string, password: string, refresh_token?: string): Promise<User>;
   find(email: string): Promise<User | null>;
+  addRefreshToken(id: string, refresh_token: string): Promise<void>;
+  getRefreshToken(id: string): Promise<string | null>;
 }
