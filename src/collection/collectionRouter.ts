2bc57a35af35ba28875b5fcd3a3a1bdcdf2911e0
José Carlos <jose.carlos@fdez.se>
2025-01-17 20:47:25 +0100

    Agregar rutas para obtener todas las colecciones y una colección específica en el enrutador de colecciones

--- src/collection/collectionRouter.ts
+++ src/collection/collectionRouter.ts
@@ -1,6 +1,9 @@
 import { Elysia } from "elysia";
 import { createCollectionController } from "@/server/dependecies";
+import { getAllCollectionController } from "@/server/dependecies";
 import { createCollectionDTO } from "./domain/collectionDTO";
+import { getCollectionDTO } from "./domain/collectionDTO";
+import { getCollectionController } from "@/server/dependecies"; 
 import { jwt } from '@elysiajs/jwt'
 
 export const collectionRouter = new Elysia({
@@ -12,3 +15,5 @@ export const collectionRouter = new Elysia({
     exp: '1h'
   }))
   .post('/', (ctx) => createCollectionController.run(ctx), createCollectionDTO)
+  .get('/', (ctx) => getAllCollectionController.run(ctx))
+  .get('/:id', (ctx) => getCollectionController.run(ctx), getCollectionDTO)
