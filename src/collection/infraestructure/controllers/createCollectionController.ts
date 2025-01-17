4baa75ebfe6f8730135f12722ec8aadf1edc07c5
José Carlos <jose.carlos@fdez.se>
2025-01-17 19:41:32 +0100

    Agregar controlador CreateCollection para gestionar la creación de colecciones

---
+++ src/collection/infraestructure/controllers/createCollectionController.ts
@@ -0,0 +1,47 @@
+import { CreateCollection } from "@/collection/application/create";
+
+export class CreateCollectionController {
+  constructor(private createCollection: CreateCollection) {}
+  async run({ body, set, access, headers }: { body: {name: string, description?: string}, set: any, access: any, headers: any}) {
+    try {
+
+      const token = headers["authorization"].split(" ")[1];
+
+      if (!token) {
+        set.status = 401;
+        return {
+          message: "Unauthorized"
+        }
+      }
+
+      const { id } = await access.verify(token);
+
+      if (id !== 'ccaa43b3-ba50-4261-a181-0cc9fc5ccad8') {
+        set.status = 401;
+        return {
+          message: "Unauthorized"
+        }
+      }
+      
+
+      const collection = await this.createCollection.run(body.name, body.description);
+      set.status = 201;
+      return {
+        data: {
+          collection: collection
+        }
+      }
+    } catch (e: any) {
+      if (e.code === "P2002") {
+        set.status = 409;
+        return {
+          message: "Collection already exists"
+        }
+      }
+      set.status = 500;
+      return {
+        message: e.message
+      }
+    }
+  }
+}
