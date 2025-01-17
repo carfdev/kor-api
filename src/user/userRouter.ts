901fb6fab94890e6c00112ab9e5b91b25ca2eaf7
José Carlos <jose.carlos@fdez.se>
2025-01-17 22:54:33 +0100

    Agregar DTOs para el inicio de sesión y la actualización del controlador de usuario

--- src/user/userRouter.ts
+++ src/user/userRouter.ts
@@ -2,7 +2,7 @@ import { Elysia } from "elysia";
 import { createUserController } from "@/server/dependecies";
 import { loginUserController } from "@/server/dependecies";
 import { refreshTokenController } from "@/server/dependecies";
-import { createUserDTO } from "./domain/userDTO";
+import { createUserDTO, loginUserDTO, refreshUserDTO } from "./domain/userDTO";
 import { jwt } from '@elysiajs/jwt'
 export const userRouter = new Elysia({
   prefix: "/user"
@@ -17,7 +17,8 @@ export const userRouter = new Elysia({
     secret: process.env.JWT_SECRET_REFRESH as string,
     exp: '30d'
   }))
+  // @ts-ignore
   .post('/register', (ctx) => createUserController.run(ctx), createUserDTO)
-  .post('/login', (ctx) => loginUserController.run(ctx), createUserDTO)
-  .get('/refresh', (ctx) => refreshTokenController.run(ctx))
-  .get('/', () => "Get all users")
+  // @ts-ignore
+  .post('/login', (ctx) => loginUserController.run(ctx), loginUserDTO)
+  .get('/refresh', (ctx) => refreshTokenController.run(ctx), refreshUserDTO)
