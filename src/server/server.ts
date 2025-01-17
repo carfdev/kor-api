8ee46bce6b226deacd2b8a72fd7c0c5e7b28e8ee
José Carlos <jose.carlos@fdez.se>
2025-01-17 22:54:27 +0100

    Agregar configuración de Swagger para la documentación de la API

--- src/server/server.ts
+++ src/server/server.ts
@@ -1,12 +1,26 @@
 import { Elysia } from "elysia";
 import { userRouter } from "@/user/userRouter";
 import { collectionRouter } from "@/collection/collectionRouter";
+import { swagger } from '@elysiajs/swagger'
 
 export class Server {
   private app: Elysia;
 
   constructor() {
     this.app = new Elysia()
+    this.app.use(swagger({
+      path: '/v1/swagger',
+      documentation: {
+        info: {
+            title: 'API Driving Theory Practice',
+            version: '1.0.0'
+        },
+        tags: [
+          { name: 'Auth', description: 'Authentication endpoints' },
+          { name: 'Collection', description: 'Collection endpoints' },
+        ]
+    }
+  }))
     this.app.group('/api/v1', (app) => 
       app
         .use(userRouter)
