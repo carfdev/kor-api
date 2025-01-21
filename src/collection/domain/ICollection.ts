013a9a91b8e5782f736c66cf62dcfa5d17b2855c
José Carlos <jose.carlos@fdez.se>
2025-01-21 13:05:15 +0100

    Modificar método de actualización en ICollection para permitir nombres opcionales

--- src/collection/domain/ICollection.ts
+++ src/collection/domain/ICollection.ts
@@ -4,6 +4,6 @@ export interface ICollection {
   create(name: string, description?: string): Promise<Collection>;
   find(id: number): Promise<Collection | null>;
   findAll(): Promise<Collection[]>;
-  update(id: number, name: string, description?: string): Promise<Collection | null>;
+  update(id: number, name?: string, description?: string): Promise<Collection | null>;
   delete(id: number): Promise<Collection | null>;
 }
