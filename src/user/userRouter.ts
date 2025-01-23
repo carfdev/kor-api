3e4afb2e3d6ec3edd6f25fb89103b4aa23479563
José Carlos <jose.carlos@fdez.se>
2025-01-23 18:43:02 +0100

    Agregar controladores y rutas para actualizar y restablecer contraseñas de usuario

--- src/user/userRouter.ts
+++ src/user/userRouter.ts
@@ -1,12 +1,15 @@
 import { Elysia } from "elysia";
-import { createUserController } from "@/server/dependecies";
-import { loginUserController } from "@/server/dependecies";
-import { refreshTokenController } from "@/server/dependecies";
-import { createUserDTO, loginUserDTO, refreshUserDTO } from "./domain/userDTO";
+import { createUserController, loginUserController, refreshTokenController, updatePasswordController, resetPasswordController } from "@/server/dependecies";
+import { createUserDTO, loginUserDTO, refreshUserDTO, updatePasswordDTO, resetPasswordDTO } from "./domain/userDTO";
 import { jwt } from '@elysiajs/jwt'
 export const userRouter = new Elysia({
   prefix: "/user"
 })
+  .use(jwt({
+    name: "update",
+    secret: process.env.JWT_SECRET_UPDATE as string,
+    exp: '15m'
+  }))
   .use(jwt({
     name: "access",
     secret: process.env.JWT_SECRET_ACCESS as string,
@@ -23,3 +26,7 @@ export const userRouter = new Elysia({
   .post('/login', (ctx) => loginUserController.run(ctx), loginUserDTO)
   // @ts-ignore
   .get('/refresh', (ctx) => refreshTokenController.run(ctx), refreshUserDTO)
+  // @ts-ignore
+  .patch('/update-password/:token', (ctx) => updatePasswordController.run(ctx), updatePasswordDTO)
+  // @ts-ignore
+  .post('/reset-password', (ctx) => resetPasswordController.run(ctx), resetPasswordDTO)
