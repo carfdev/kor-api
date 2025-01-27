5a09c51c3476274e32bbabdc41d11c2304ebf72f
José Carlos <jose.carlos@fdez.se>
2025-01-27 12:44:50 +0100

    Actualizar la clase Group para permitir una descripción nula en lugar de un arreglo vacío

--- src/group/domain/Group.ts
+++ src/group/domain/Group.ts
@@ -1,15 +1,10 @@
-import { Question } from "@/question/domain/Question";
-import { Collection } from "@/collection/domain/Collection";
-
 export class Group {
   constructor(
     public id: number,
     public name: string,
     public collectionId: number,
-    public collection: Collection,
-    public questions: Question[],
     public createdAt: Date,
     public updatedAt: Date,
-    public description?: string
+    public description?: string | null
   ) {}
 }
